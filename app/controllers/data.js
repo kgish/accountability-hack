73c24bb79b4c2898ba6b05d67258237a76c135c4
Kiffin Gish <kiffin.gish@planet.nl>
2017-06-07 23:48:53 +0200

    Select government kinds.

--- app/controllers/data.js
+++ app/controllers/data.js
@@ -1,13 +1,45 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    years: null,
-    year: null,
+    years: [],
+    year: 0,
+
+    periods: [
+        { value: 0, text: 'All' },
+        { value: 1, text: '1st quarter' },
+        { value: 2, text: '2nd quarter' },
+        { value: 3, text: '3rd quarter' },
+        { value: 4, text: '4th quarter' }
+    ],
+    period: 0,
+
+    kinds: [],
+    kind: 'Please select',
+
+    disabled: Ember.computed('kind', function(){
+       let kind = this.get('kind');
+       return kind === 'Please select' ? ' disabled' : '';
+    }),
 
     actions: {
         selectYear(year) {
             console.log('selectYear('+year+')');
-
+            this.set('year', year);
+        },
+        selectPeriod(period) {
+            console.log('selectPeriod('+period+')');
+            this.set('period', period);
+        },
+        selectKind(kind) {
+            console.log('selectKind('+kind+')');
+            this.set('kind', kind);
+            Ember.$('#kind option:contains("Please select")').remove();
+        },
+        submit() {
+            let year = this.get('year'),
+                period = this.get('period'),
+                kind = this.get('kind');
+            console.log('submit() year=' + year + ', period=' + period + ', kind=' + kind);
         }
     }
 });
